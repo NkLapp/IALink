42872e16db23a6ce7e215c6e817003176645a2a6
NkLapp <34657611+NkLapp@users.noreply.github.com>
2024-03-16 21:35:49 +0200

    springboot initializer start

---
+++ src/main/java/com/ialink/ialink/IalinkApplication.java
@@ -0,0 +1,13 @@
+package com.ialink.ialink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IalinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IalinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ialink/ialink/IalinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ialink.ialink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IalinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
